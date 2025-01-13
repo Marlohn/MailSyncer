1b32feff2683fe8b182e4fb463b0925246202aec
Marlohn Choinski <marlohn.choinski@gmail.com>
2025-01-12 23:06:42 -0300

    scalar instalation

--- MailSyncer/Presentation/MailSyncer.API/Program.cs
+++ MailSyncer/Presentation/MailSyncer.API/Program.cs
@@ -2,6 +2,7 @@ using MailSyncer.API.Middlewares;
 using MailSyncer.Infrastructure.Adapters.Mailchimp.Settings;
 using MailSyncer.Infrastructure.HttpClients.Settings;
 using MailSyncer.IoC;
+using Scalar.AspNetCore;
 
 namespace MailSyncer.API
 {
@@ -10,6 +11,7 @@ namespace MailSyncer.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddOpenApi();
 
             // Register settings
             builder.Services.Configure<MailchimpSettings>(builder.Configuration.GetSection("Mailchimp"));
@@ -28,10 +30,21 @@ namespace MailSyncer.API
             app.UseMiddleware<ExceptionHandlingMiddleware>();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            //if (app.Environment.IsDevelopment())
+
+            app.Use(async (context, next) =>
             {
-                app.MapOpenApi();
-            }
+                if (context.Request.Path == "/")
+                {
+                    context.Response.Redirect("/scalar/v1");
+                    return;
+                }
+                await next();
+            });
+
+            app.MapOpenApi();
+            app.MapScalarApiReference();
+
 
             app.UseHttpsRedirection();
 
